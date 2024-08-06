ca410b4aa3578d254ace33e5dfe7bde2b6c1a7c0
abhishek <abhisheksara21@gmail.com>
2024-08-06 22:33:25 +0530

    corrected leetcode 88 code

--- leetcode_python_88.py
+++ leetcode_python_88.py
@@ -5,21 +5,24 @@ You are given two integer arrays nums1 and nums2, sorted in non-decreasing order
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 """
+# IndexError: list index out of range
+
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     """
     Do not return anything, modify nums1 in-place instead.
     """
-    l = m+n-1
-    while n>0:
-        if nums1[m-1]>nums2[n-1]:
-            nums1[l] = nums1[m-1]
-            m = m-1
+    i, j = m-1, n-1
+    k = m+n-1
+    while j>=0:
+        if i>=0 and nums1[i]>=nums2[j]: # I got index error coz i forgot to check if i is less than 0
+            nums1[k] = nums1[i]
+            i-=1
         else:
-            nums1[l] = nums2[n-1]
-            n = n-1
-        l = l-1
+            nums1[k] = nums2[j]
+            j-=1
+        k-=1
 
 if __name__=="__main__":
     test_cases = [
